987378ccca8a8e4de8bf62687fe9e53af06670fa
jerry7657 <jerry7657@gmail.com>
2020-10-14 21:09:56 +0800

    Updates

---
+++ precache-manifest.ae1d039db0ff99501fb4cfbc3292306f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47845099a8a7594fb3f0bc952c4a385d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ee9833048d691256819e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9833048d691256819e",
+    "url": "/static/js/main.99196d02.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
